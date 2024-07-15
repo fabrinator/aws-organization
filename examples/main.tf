fa42a199b01b3becdc0c4a460f0bc9c73886520e
Fabri Rivas <info@fabririvas.com>
2024-07-15 09:58:18 -0400

    ADD: New account for shared services

--- examples/main.tf
+++ examples/main.tf
@@ -1,12 +1,23 @@
 module "organization" {
-  source = "../"
+  source                        = "../"
   aws_service_access_principals = ["sso.amazonaws.com"]
-  feature_set = "ALL"
+  feature_set                   = "ALL"
 
   organization = {
     units = [
       {
         name = "infrastructure"
+        units = [
+          {
+            name = "shared",
+            accounts = [
+              {
+                name  = "infra-shared"
+                email = "fabri1492+aws+shared@hotmail.com"
+              }
+            ]
+          }
+        ]
       },
       {
         name = "crc"
@@ -15,7 +26,7 @@ module "organization" {
             name = "bootcamp",
             accounts = [
               {
-                name = "bootcamp-account"
+                name  = "bootcamp-account"
                 email = "fabri1492+aws+bootcamp@hotmail.com"
               }
             ]
@@ -43,4 +54,4 @@ module "organization" {
     ]
   }
 }
- 
+
